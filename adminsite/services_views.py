d06065c19036cca2bb07f986fc41c5610ebf2725
julesgtz <113105305+julesgtz@users.noreply.github.com>
2023-06-12 14:18:30 +0200

    Update services_views.py

--- adminsite/services_views.py
+++ adminsite/services_views.py
@@ -10,6 +10,30 @@ def ajout(request):
 def traitement(request):
     form = ServicesForms(request.POST)
     if form.is_valid():
+        donnees = form.save(commit=False)
+        stockage = donnees.stockage_use
+        vive = donnees.memoire_vive
+        serveur = models.Serveurs.objects.get(pk=donnees.serveur_lancement.id)
+        stockage_serveur = serveur.stockage
+        memoire_serveur = serveur.memoire
+        if serveur.stockage.endswith("Go"):
+            serveur.stockage = int(serveur.stockage[:-2]) * 1_000
+        elif serveur.stockage.endswith("To"):
+            serveur.stockage = int(serveur.stockage[:-2]) * 1_000_000
+
+        donnees.stockage_use = int(donnees.stockage_use)
+        serveur.stockage = int(serveur.stockage)
+
+        if donnees.stockage_use>serveur.stockage:
+            "erreur pas assez de stockage"
+        else:
+            donnees.stockage_use = serveur.stockage - donnees.stockage_use
+            donnees.stockage_use = str(donnees.stockage_use)
+            print(donnees.serveur_lancement.id)
+            serveur_update = models.Serveurs.objects.get(id=donnees.serveur_lancement.id)
+            serveur_update.stockage = donnees.stockage_use
+            serveur_update.save()
+
         app = form.save()
         return HttpResponseRedirect("/adminsite/services/")
     else:
@@ -20,6 +44,8 @@ def index(request):
     return render(request, 'adminsite/services/affiche.html',{"liste": app})
 
 def delete(request, id):
+
+    # quand on supprime un services, rajouter au serveur l'espace qu'il utilisait
     stream = models.Services.objects.get(pk=id)
     stream.delete()
     return HttpResponseRedirect("/adminsite/services/")
