bc13a61875dd036fa91b0b3238aaa8acab38837f
nathanpigeyre <118524714+nathanpigeyre@users.noreply.github.com>
2023-06-12 10:31:42 +0200

    Update models.py
    anat

--- adminsite/models.py
+++ adminsite/models.py
@@ -23,7 +23,7 @@ class Serveurs(models.Model):
     stockage = models.CharField(max_length=20, choices=stockage_choices, null=True)
 
     def __str__(self):
-        return f" {self.nom} {self.type} avec un processeur {self.processeur} et une memoire de {self.memoire} avec un stockage de {self.stockage}"
+        return f"{self.nom}"
 
     def dico(self):
         return {"nom": self.nom, "type": self.type,"processeur": self.processeur,"memoire": self.memoire,"stockage": self.stockage}
@@ -35,7 +35,7 @@ class Utilisateurs(models.Model):
     email = models.EmailField(max_length=50)
 
     def __str__(self):
-        return f" {self.nom} {self.prenom}, {self.email}"
+        return f"{self.nom} {self.prenom}"
 
     def dico(self):
         return {"nom": self.nom, "prenom": self.prenom,"email": self.email}
@@ -58,12 +58,13 @@ class Applications(models.Model):
 class Services(models.Model):
     nom = models.CharField(max_length=30)
     date = models.DateField()
-    stockage_use = models.CharField(max_length=8)
+    memoire_use = models.CharField(max_length=8)
     memoire_vive = models.CharField(max_length=8)
     serveur_lancement = models.ForeignKey(Serveurs, on_delete=models.CASCADE, null=True, related_name="serveur_lancement")
+
     def __str__(self):
-        return f" {self.nom},le {self.date} utilisant {self.stockage_use} de stockage , nécessitant {self.memoire_vive} de mémoire , sur le serveur : {self.serveur_lancement}"
+        return f"{self.nom},le {self.date} utilisant {self.memoire_use} de memoire , nécessitant {self.memoire_vive} de mémoire , sur le serveur {self.serveur_lancement}"
 
     def dico(self):
-        return {"nom": self.nom, "date": self.date,"memoire_use": self.stockage_use,"stockage_use": self.memoire_vive,"serveur_lancement": self.serveur_lancement}
+        return {"nom": self.nom, "date": self.date,"memoire_use": self.memoire_use,"memoire_vive": self.memoire_vive,"serveur_lancement": self.serveur_lancement}
 
