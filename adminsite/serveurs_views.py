8d8273699f313337c97714b9096d7b5dfad71276
Nathan Pigeyre <nathan.pigeyre@uha.fr>
2023-06-12 15:35:10 +0200

    fpdf change

--- adminsite/serveurs_views.py
+++ adminsite/serveurs_views.py
@@ -50,34 +50,38 @@ def serveur_pdf(request,id):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=20)
-    pdf.cell(200, 10, txt="RAPPORT SUR LE SERVEUR : ", ln=1, align='C')
-    pdf.cell(200, 10, txt=serveur.nom, ln=1, align='C')
+    pdf.set_text_color(0, 0, 255)
+    pdf.cell(200, 10,  ln=1, align='C')
+    pdf.cell(200, 10, txt="RAPPORT SUR LE SERVEUR : " + serveur.nom, ln=1, align='C')
+    pdf.cell(200, 10,  ln=1, align='C')
     pdf.set_font("Arial", size=15)
-    pdf.cell(200, 10, txt="Le serveur " + serveur.nom + " est un serveur de type " + models.Types.objects.get(
-        pk=serveur.type.id).type, ln=2, align='C')
-    pdf.cell(200, 10, txt="Espace stockage: " + str(serveur.stockage), ln=4, align='C')
-    pdf.cell(200, 10, txt="Espace restant: " + "Mo", ln=5, align='C')
-    pdf.cell(200, 10, txt="Memoire vive: " + str(serveur.memoire), ln=6, align='C')
-    pdf.cell(200, 10, txt="Memoire vive non occupée: " + "Mo", ln=7, align='C')
-    pdf.cell(200, 10, txt="Processeur: " + str(serveur.processeur), ln=8, align='C')
+    pdf.cell(200, 10, txt="* Le serveur " + serveur.nom + " est un serveur de type " + models.Types.objects.get(
+        pk=serveur.type.id).type, ln=2, align='L')
+    pdf.cell(200, 10, txt="* Espace stockage: " + str(serveur.stockage), ln=4, align='L')
+    pdf.cell(200, 10, txt="* Espace restant: " + "Mo", ln=5, align='L')
+    pdf.cell(200, 10, txt="* Memoire vive: " + str(serveur.memoire), ln=6, align='L')
+    pdf.cell(200, 10, txt="* Memoire vive non occupée: " + "Mo", ln=7, align='L')
+    pdf.cell(200, 10, txt="* Processeur: " + str(serveur.processeur), ln=8, align='L')
     line = 9
+    pdf.cell(200, 10,  ln=1, align='C')
     if len(list(models.Applications.objects.filter(serveur=serveur))) == 0:
-        pdf.cell(200, 10, txt="Il n'y a aucune application sur ce serveur.", ln=line, align='C')
+        pdf.cell(200, 10, txt="Il n'y a aucune application sur ce serveur.", ln=line, align='L')
         line = line + 1
     else:
-        pdf.cell(200, 10, txt="Les applications sur ce serveur:", ln=line, align='C')
+        pdf.cell(200, 10, txt="* Les applications sur ce serveur:", ln=line, align='L')
         line = line + 1
         for i in list(models.Applications.objects.filter(serveur=serveur)):
-            pdf.cell(200, 10, txt=i.nom, ln=line, align='C')
+            pdf.cell(200, 10, txt='         - ' +i.nom, ln=line, align='L')
             line = line + 1
+    pdf.cell(200, 10,  ln=1, align='C')        
     if len(list(models.Services.objects.filter(serveur_lancement=serveur))) == 0:
-        pdf.cell(200, 10, txt="Il n'y a aucun service sur ce serveur.", ln=line, align='C')
+        pdf.cell(200, 10, txt="Il n'y a aucun service sur ce serveur.", ln=line, align='L')
         line = line + 1
     else:
-        pdf.cell(200, 10, txt="Les services sur ce serveur:", ln=line, align='C')
+        pdf.cell(200, 10, txt="* Les services sur ce serveur:", ln=line, align='L')
         line = line + 1
         for i in list(models.Services.objects.filter(serveur_lancement=serveur)):
-            pdf.cell(200, 10, txt=i.nom, ln=line, align='C')
+            pdf.cell(200, 10, txt='         -'+i.nom, ln=line, align='L')
             line = line + 1
     pdf.output("Serveur-" + str(id) + ".pdf")
     response = FileResponse(open("Serveur-" + str(id) + ".pdf", 'rb'))
